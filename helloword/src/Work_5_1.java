7610085e57a077e55edc61174783357555db6ff1
xuzengwen <874844850@qq.com>
2019-02-28 20:10:05 +0800

    修改代码，添加查询最大值

--- helloword/src/Work_5_1.java
+++ helloword/src/Work_5_1.java
@@ -3,6 +3,8 @@ public class Work_5_1 {
         int a=4;
         int b=3;
         int num=(a>b)?a+b:a*b;
+        int max=(a>b)?a:b;
+        System.out.println(max);
         System.out.println(num);
     }
 }
