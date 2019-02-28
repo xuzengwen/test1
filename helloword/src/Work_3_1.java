57fdbcc8287b766f25a7f9e63db17f836c3c9f4c
xuzengwen <874844850@qq.com>
2019-02-28 19:55:43 +0800

    修改a变量的值

--- helloword/src/Work_3_1.java
+++ helloword/src/Work_3_1.java
@@ -1,6 +1,6 @@
 public class Work_3_1 {
     public static void main(String[] args) {
-        int a=534;
+        int a=680;
         int bai=a/100%10;
         int shi=a/10%10;
         int ge=a%10;
