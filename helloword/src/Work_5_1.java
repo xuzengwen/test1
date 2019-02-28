bc74de356a98a029c304578016f69c82bd02213d
xuzengwen <874844850@qq.com>
2019-02-28 20:07:10 +0800

    修改work_5_1的变量值

--- helloword/src/Work_5_1.java
+++ helloword/src/Work_5_1.java
@@ -1,6 +1,6 @@
 public class Work_5_1 {
     public static void main(String[] args) {
-        int a=4;
+        int a=6;
         int b=3;
         int num=(a>b)?a+b:a*b;
         System.out.println(num);
